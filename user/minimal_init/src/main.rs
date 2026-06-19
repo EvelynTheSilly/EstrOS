5c2792b3d966523b7d8fdf2dfa9060323d1106a3
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-06-19 17:04:50 +0100

    implement stack and run first proper rust code

--- user/minimal_init/src/main.rs
+++ user/minimal_init/src/main.rs
@@ -1,25 +1,39 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::naked_asm, panic::PanicInfo};
+use core::{
+    arch::{asm, naked_asm},
+    panic::PanicInfo,
+};
 
 #[unsafe(no_mangle)]
 #[unsafe(naked)]
 extern "C" fn _start() {
     naked_asm!(
         "
-        adr x0, _hello
-        mov x1, #23
-        svc #1
-        svc #1
+        ldr x0, =_stack
+        mov sp, x0
+        bl {}
         b .
-    _hello:
-        .ascii \"\nHello from userspace!\\n\"
         ",
+        sym main
     );
 }
 
 #[panic_handler]
 fn panic_handler(_: &PanicInfo) -> ! {
     loop {}
+}
+const HELLO_FROM_RUST: &str = "haiii, from rust this time";
+
+fn main() {
+    unsafe {
+        asm!(
+            "
+            svc #1
+            ",
+            in("x0") HELLO_FROM_RUST.as_ptr(),
+            in("x1") HELLO_FROM_RUST.len()
+        );
+    }
 }
