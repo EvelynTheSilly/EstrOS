2ab1579e4aa7603397aa5a29624efe925d27ab37
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-08-03 14:20:00 +0100

    some lib c stuff

--- flake.nix
+++ flake.nix
@@ -7,16 +7,18 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    rust-overlay,
-  }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         cross = pkgs.pkgsCross.aarch64-embedded;
-        overlays = [rust-overlay.overlays.default];
+        overlays = [ rust-overlay.overlays.default ];
         pkgs = import nixpkgs {
           inherit system overlays;
           config.allowUnsupportedSystem = true;
@@ -27,40 +29,67 @@
 
         rust = pkgs.rust-bin.fromRustupToolchain {
           channel = toolchain.channel;
-          components = toolchain.components or [];
-          targets = toolchain.targets or [];
+          components = toolchain.components or [ ];
+          targets = toolchain.targets or [ ];
         };
 
-        aarch64-estros-binutils = pkgs.wrapCCWith {
-          cc = cross.buildPackages.gcc;
+        sysroot = ./lib/c/sysroot;
+
+        estros-gcc = cross.buildPackages.wrapCCWith {
+          cc = cross.buildPackages.gcc.cc;
           bintools = cross.buildPackages.binutils;
           extraTools = with cross.buildPackages; [
             binutils
             binutils-unwrapped
             elfutils
           ];
+          extraBuildCommands = ''
+            echo "-isystem ${sysroot}/include" >> $out/nix-support/cc-cflags
+            echo "-L ${sysroot}/lib" >> $out/nix-support/cc-ldflags
+            cat > $out/nix-support/estros.specs <<EOF
+*startfile:
+%{!shared: ${sysroot}/lib/crt0.o}
+*lib:
+%{!shared: -lestros}
+EOF
+            echo "-static -specs=$out/nix-support/estros.specs" >> $out/nix-support/cc-cflags
+          '';
         };
-      in {
+
+        aarch64-estros-binutils = pkgs.runCommandLocal "aarch64-estros-binutils" {
+          wrapped = estros-gcc;
+        } ''
+          mkdir -p $out/bin $out/nix-support
+          for src in $wrapped/bin/*; do
+            name=$(basename "$src")
+            ln -s "$src" "$out/bin/''${name/aarch64-none-elf-/aarch64-estros-}"
+          done
+          cat > $out/nix-support/setup-hook <<EOF
+          addToSearchPath _PATH $out/bin
+          EOF
+        '';
+      in
+      {
         packages.aarch64-estros-binutils = aarch64-estros-binutils;
 
         devShells.default = pkgs.mkShell {
-          packages =
-            [
-              pkgs.cargo-expand
-              rust
-              pkgs.bacon
-              pkgs.pkg-config
-              pkgs.openssl
-              pkgs.qemu
-              pkgs.cmake
-              pkgs.just
-              pkgs.just-lsp
-              pkgs.just-formatter
-              pkgs.cloc
-            ]
-            ++ (
-              if system != "aarch64-darwin"
-              then [
+          packages = [
+            pkgs.cargo-expand
+            rust
+            pkgs.bacon
+            pkgs.pkg-config
+            pkgs.openssl
+            pkgs.qemu
+            pkgs.cmake
+            pkgs.just
+            pkgs.just-lsp
+            pkgs.just-formatter
+            pkgs.cloc
+          ]
+          ++ (
+            if system != "aarch64-darwin" then
+              [
+                cross.buildPackages.gcc
                 aarch64-estros-binutils
                 cross.buildPackages.gdb
                 pkgs.mtools
@@ -69,10 +98,15 @@
                 pkgs.gptfdisk
                 pkgs.python313Packages.virt-firmware
               ]
-              else []
-            );
-          LIMINE_EFI_PATH = (if system != "aarch64-darwin" then "${pkgs.limine-full}/share/limine/BOOTAA64.EFI" else "");
-          BOOT_FIRMWARE_PATH = (if system != "aarch64-darwin" then "${pkgs.pkgsCross.aarch64-multiplatform.OVMF.fd}/FV" else "");
+            else
+              [ ]
+          );
+          LIMINE_EFI_PATH = (
+            if system != "aarch64-darwin" then "${pkgs.limine-full}/share/limine/BOOTAA64.EFI" else ""
+          );
+          BOOT_FIRMWARE_PATH = (
+            if system != "aarch64-darwin" then "${pkgs.pkgsCross.aarch64-multiplatform.OVMF.fd}/FV" else ""
+          );
           CARGO_UNSTABLE_JSON_TARGET_SPEC = "true";
           shellHook = ''
             if [[ $- == *i* ]]; then
